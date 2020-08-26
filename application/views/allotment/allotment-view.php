2a12cb198094eafde3e6d53126caf67bca1e9e87
Giddel Lucion <66565246+glucion05012@users.noreply.github.com>
2020-08-26 16:18:51 +0800

    added allotment db

--- application/views/allotment/allotment-view.php
+++ application/views/allotment/allotment-view.php
@@ -15,7 +15,6 @@
             <tr>
                 <th scope="col">Region</th>
                 <th scope="col">Year</th>
-                <th scope="col">Type</th>
                 <th scope="col">Status</th>
                 <th scope="col">Action</th>
             </tr>
@@ -23,20 +22,11 @@
         <tbody> 
             <?php foreach($allotments as $allotment) : ?>
                 <tr class="table-active"> 
-                    <td><?php echo $allotment['region']; ?></td>
-                    <td><?php echo $allotment['year']; ?></td>
-                    <td><?php echo $allotment['fund_source']; ?></td>
-                    <td><?php 
-                    if($allotment['status'] == 1){
-                        echo 'Current'; 
-                    }elseif ($allotment['status'] == 2){
-                        echo 'Continuing'; 
-                    }
-
-                    
-                    ?></td>
+                    <td><?php echo $allotment['all_region']; ?></td>
+                    <td><?php echo $allotment['all_year']; ?></td>
+                    <td><?php echo $allotment['all_status']; ?></td>
                     <td>
-                        <a class="btn btn-primary" href="allotment/class/<?php echo $allotment['id'] ?>">Edit</a>
+                        <a class="btn btn-primary" href="allotment/class/<?php echo $allotment['all_id'] ?>">Edit</a>
                         <a class="btn btn-danger" onclick="return confirm('Press OK to confirm delete PAP?')" href="allotment/delete/<?php echo $allotment['id'] ?>">Delete</a>
                     </td>
             </tr>   
