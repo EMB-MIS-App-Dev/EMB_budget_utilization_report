e76db0ace4756a7558b81a87d64678e9fb32334f
giddel05012 <giddel.lucion@yahoo.com>
2020-10-24 17:35:20 +0800

    utilization report

--- application/views/js/reports-js.php
+++ application/views/js/reports-js.php
@@ -150,10 +150,142 @@ $("#generatebtn").click(function(){
                     
 
             }
-        }else{
-            alert('no reports found!');
-            $('#reportDispHeader').empty();
-            $('#reportDisp').empty();
+        }else if($( "[name='report'] option:selected" ).val() == "ut"){
+            var $inputH = $(
+                    "<div style='text-align:center'>"+
+                        "<h5>"+
+                            "Budget Utilization Rate<br>" + $( "[name='category'] option:selected" ).text()+
+                        "</h5>"+
+                        "<p>as of "+ $( "[name='month_to'] option:selected" ).text()+" "+$( "[name='year']" ).val()+"</p>"+
+                    "</div>"
+            );
+
+            $('#reportDispHeader').empty().show().append($inputH);
+            
+            var $inputTH = $(
+                "<tr>"+
+                    "<th>Region</th>"+
+                    "<th>Total Allotment</th>"+
+                    "<th>Actual Obligation as of this Month</th>"+
+                    "<th>Balance in Allotment</th>"+
+                    "<th>% Utilization</th>"+
+                    "<th>% Unutilized</th>"+
+                "</tr>"
+            );
+
+            $('#reportDisp').empty(); 
+            $('#reportDisp').append($inputTH);
+
+            var regions = ["CO","NCR","R1","CAR","ARMM","R2","R3","R4A","R4B","R5","R6","R7","R8","R9","R10","R11","R12","R13"];
+                for (var r = 0; r <= 17; r++) {
+                    
+                    var region = regions[r];
+
+                    var totalAll = region+'_totalAll';
+                    var actOblThis_total = region+'_actOblThis_total';
+                    var balanceAll = region+'_balanceAll';
+                    var utilization = region+'_utilization';
+                    var unutilized = region+'_unutilized';
+
+                    totalAll = 0;
+                    actOblThis_total = 0;
+                    balanceAll = 0;
+                    utilization = 0;
+                    unutilized = 0;
+
+                    <?php foreach($allotment_amt_all as $aa) : ?>
+                        if ($("[name='category']").val() == "<?php echo $aa['all_category']; ?>"){
+                            if($("[name='year']").val() == "<?php echo $aa['all_year']; ?>"){
+                                if($("[name='class']").val() == "<?php echo $aa['all_class']; ?>"){
+
+                                    if("<?php echo $aa['all_region']; ?>" == region){
+
+                                        // Target Obligation as of this Month
+                                        var month = [Number(<?php echo $aa['amt_jan']; ?>), 
+                                                    Number(<?php echo $aa['amt_feb']; ?>),
+                                                    Number(<?php echo $aa['amt_mar']; ?>),
+                                                    Number(<?php echo $aa['amt_apr']; ?>),
+                                                    Number(<?php echo $aa['amt_may']; ?>),
+                                                    Number(<?php echo $aa['amt_jun']; ?>),
+                                                    Number(<?php echo $aa['amt_jul']; ?>),
+                                                    Number(<?php echo $aa['amt_aug']; ?>),
+                                                    Number(<?php echo $aa['amt_sep']; ?>),
+                                                    Number(<?php echo $aa['amt_oct']; ?>),
+                                                    Number(<?php echo $aa['amt_nov']; ?>),
+                                                    Number(<?php echo $aa['amt_dec']; ?>)
+                                                    ];
+
+                                        //Target
+                                        for (var i = 0; i <= 11; i++) {
+                                            totalAll += Number(month[i]);   
+                                        }
+                                        
+
+                                        // Actual Obligation as of this Month
+                                        <?php foreach($obligation as $ob) : ?>
+                                            if("<?php echo $aa['amt_id']; ?>" == "<?php echo $ob['ob_amt_id']; ?>"){
+
+                                                var from = Number($("[name='month_from']").val());
+                                                var to = Number($("[name='month_to']").val());
+                                                
+                                                var myMonth = <?php echo $ob['ob_month']; ?>;
+
+                                                if (myMonth >= from && myMonth <= to){
+                                                    actOblThis_total += Number(<?php echo $ob['ob_amount']; ?>);
+                                                }
+                                            }
+                                        <?php endforeach; ?>
+                                    }
+                                }
+                            }   
+                        }
+                    <?php endforeach; ?>
+
+                    // balanceAll
+                    balanceAll = totalAll-actOblThis_total;
+
+                    // utilization
+                    utilization = (actOblThis_total/totalAll) * 100;
+                    utilization = Math.floor(utilization); 
+
+                    // unutilized
+                    unutilized = (100-utilization);
+                    unutilized = Math.floor(unutilized); 
+
+                    totalAll = totalAll.toFixed(2)
+                                .replace(/[^\d.]/g, "")
+                                .replace(/^(\d*\.)(.*)\.(.*)$/, '$1$2$3')
+                                .replace(/\.(\d{2})\d+/, '.$1')
+                                .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+                    actOblThis_total = actOblThis_total.toFixed(2)
+                                .replace(/[^\d.]/g, "")
+                                .replace(/^(\d*\.)(.*)\.(.*)$/, '$1$2$3')
+                                .replace(/\.(\d{2})\d+/, '.$1')
+                                .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+                    balanceAll = balanceAll.toFixed(2)
+                                .replace(/[^\d.]/g, "")
+                                .replace(/^(\d*\.)(.*)\.(.*)$/, '$1$2$3')
+                                .replace(/\.(\d{2})\d+/, '.$1')
+                                .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    
+                    var $input = $(
+                            "<tr>"+
+                                "<td>"+region+"</td>"+
+                                "<td>"+totalAll+"</td>"+
+                                "<td>"+actOblThis_total+"</td>"+
+                                "<td>"+balanceAll+"</td>"+
+                                "<td>"+utilization+"%</td>"+
+                                "<td>"+unutilized+"%</td>"+
+                            "</tr>"
+                    );
+                
+                    $('#reportDisp').show().append($input);
+                    
+
+            }
+
         }
     }
 
